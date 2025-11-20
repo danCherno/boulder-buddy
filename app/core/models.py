0108da381d47bee7dbc9930bd4726c1f1fe8d5b2
Cherno <daniel.cherno9@gmail.com>
2025-11-20 08:41:18 +0200

    initial commit

---
+++ app/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+"""
+class Sample(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+"""
+
---
+++ app/core/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics, permissions
+# from .models import Sample
+# from .serializers import SampleSerializer
+
+"""
+class sampleView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    queryset = Sample.objects.all().order_by('-published_date')
+    serializer_class = SampleSerializer
+"""
